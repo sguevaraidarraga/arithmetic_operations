f5073d27e724629d5fc076a59c8e1f43d22a8f29
Santiago Guevara Idarraga <143007178+sguevaraidarraga@users.noreply.github.com>
2023-10-21 23:01:28 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,12 +4,7 @@ def game():
     score = 0
     while True:
         print('======== Menu ========'
-              '\n1. Addition'
               '\n2. Subtraction'
-              '\n3. Multiplication'
-              '\n4. Division'
-              '\n5. Power'
-              '\n6. Modulo'
               '\n0. Exit')
         option = int(input('\nChoice an option: '))
 
@@ -17,43 +12,8 @@ def game():
         num_2 = input('Enter second number: ')
         answer = int(input('Enter you answer: '))
 
-        if option == 1:
-            result = addition(num_1, num_2)
-            if result == answer:
-                score += 1
-                print('Correct!!')
-            else:
-                print('Incorrect')
-        elif option == 2:
+        if option == 2:
             result = subtraction(num_1, num_2)
-            if result == answer:
-                score += 1
-                print('Correct!!')
-            else:
-                print('Incorrect')
-        elif option == 3:
-            result = multiplication(num_1, num_2)
-            if result == answer:
-                score += 1
-                print('Correct!!')
-            else:
-                print('Incorrect')
-        elif option == 4:
-            result = division(num_1, num_2)
-            if result == answer:
-                score += 1
-                print('Correct!!')
-            else:
-                print('Incorrect')
-        elif option == 5:
-            result = power(num_1, num_2)
-            if result == answer:
-                score += 1
-                print('Correct!!')
-            else:
-                print('Incorrect')
-        elif option == 1:
-            result = modulo(num_1, num_2)
             if result == answer:
                 score += 1
                 print('Correct!!')
