86fc4171e30cbd2e5573fcff1a7ba7378fb8babf
Santiago Guevara Idarraga <143007178+sguevaraidarraga@users.noreply.github.com>
2023-10-21 23:01:38 -0500

    Update operations.py

--- operations.py
+++ operations.py
@@ -1,29 +1,4 @@
-def addition(num1, num2):
-    result = num1 + num2
-    print(f'{num1} + {num2} is equal to {result}')
-    return result
-
 def subtraction(num1, num2):
     result = num1 - num2
     print(f'{num1} - {num2} is equal to {result}')
-    return result
-
-def multiplication(num1, num2):
-    result = num1 * num2
-    print(f'{num1} * {num2} is equal to {result}')
-    return result
-
-def division(num1, num2):
-    result = num1 / num2
-    print(f'{num1} / {num2} is equal to {result}')
-    return result
-
-def power(num1, num2):
-    result = num1 ^ num2
-    print(f'{num1} ^ {num2} is equal to {result}')
-    return result
-
-def modulo(num1, num2):
-    result = num1 % num2
-    print(f'{num1} % {num2} is equal to {result}')
     return result
