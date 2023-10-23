a37879f65d1a92c96f0fa12c9df6fd2f78a26048
Santiago Guevara Idarraga <143007178+sguevaraidarraga@users.noreply.github.com>
2023-10-22 19:17:43 -0500

    Add files via upload

---
+++ init_.py
@@ -0,0 +1,33 @@
+from operations import add
+
+def game():
+    score = 0
+    while True:
+        print('======== Menu ========'
+              '\n1. Sum'
+              '\n2. Rest'
+              '\n3. Mult'
+              '\n4. Div'
+              '\n5. Pot'
+              '\n6. Mod'
+              '\n0. Exit')
+        option = int(input('\nChoice an option: '))
+        if option == 0:
+            break
+        num_1 = input('Enter first number: ')
+        num_2 = input('Enter second number: ')
+        answer = int(input('Enter you answer: '))
+
+        if option == 1:
+            result = add(num_1, num_2)
+            if result == answer:
+                score += 1
+                print('Correct!!')
+            else:
+                print('Incorrect')
+
+    print(f'======== Game Over ========'
+          f'\nYou score is {score}'
+          '\nKeep going!')
+
+game()
---
+++ operation.py
@@ -0,0 +1,4 @@
+def add(num_1, num_2):
+    result = int(num_1) + int(num_2)
+    print(f'{num_1} + {num_2} is equal to {result}')
+    return result
