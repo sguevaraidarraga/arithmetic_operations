dc79254b3c7984340c592d8476f6e7cc296f27b0
Santiago Guevara Idarraga <143007178+sguevaraidarraga@users.noreply.github.com>
2023-10-22 19:19:32 -0500

    Update operation.py

--- operation.py
+++ operation.py
@@ -2,3 +2,8 @@ def add(num_1, num_2):
     result = int(num_1) + int(num_2)
     print(f'{num_1} + {num_2} is equal to {result}')
     return result
+
+def resta(num_1, num_2):
+    result = int(num_1) - int(num_2)
+    print(f'{num_1} - {num_2} is equal to {result}')
+    return result
