f56a4ae87844f5de8486032a7801a26cfe398fb4
Santiago Guevara Idarraga <143007178+sguevaraidarraga@users.noreply.github.com>
2023-10-31 07:35:45 -0500

    Update init_.py

--- init_.py
+++ init_.py
@@ -1,4 +1,4 @@
-from operations import add
+from operations import add, resta, multiplicacion, division, potencia, modulo
 
 def game():
     score = 0
@@ -20,6 +20,41 @@ def game():
 
         if option == 1:
             result = add(num_1, num_2)
+            if result == answer:
+                score += 1
+                print('Correct!!')
+            else:
+                print('Incorrect')
+         if option == 2:
+            result = resta(num_1, num_2)
+            if result == answer:
+                score += 1
+                print('Correct!!')
+            else:
+                print('Incorrect')
+         if option == 3:
+            result = multiplicacion(num_1, num_2)
+            if result == answer:
+                score += 1
+                print('Correct!!')
+            else:
+                print('Incorrect')
+        if option == 4:
+            result = division(num_1, num_2)
+            if result == answer:
+                score += 1
+                print('Correct!!')
+            else:
+                print('Incorrect')
+        if option == 5:
+            result = potencia(num_1, num_2)
+            if result == answer:
+                score += 1
+                print('Correct!!')
+            else:
+                print('Incorrect')
+        if option == 6:
+            result = modulo(num_1, num_2)
             if result == answer:
                 score += 1
                 print('Correct!!')
