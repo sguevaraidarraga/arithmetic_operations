a0bed7c365ef3860f134cebd8d2c4299d5ad45e7
Santiago Guevara Idarraga <143007178+sguevaraidarraga@users.noreply.github.com>
2023-10-31 07:30:53 -0500

    Update operation.py

--- operation.py
+++ operation.py
@@ -2,28 +2,3 @@ def add(num_1, num_2):
     result = int(num_1) + int(num_2)
     print(f'{num_1} + {num_2} is equal to {result}')
     return result
-
-def resta(num_1, num_2):
-    result = int(num_1) - int(num_2)
-    print(f'{num_1} - {num_2} is equal to {result}')
-    return result
-
-def multiplicacion(num_1, num_2):
-    result = int(num_1) * int(num_2)
-    print(f'{num_1} * {num_2} is equal to {result}')
-    return result
-
-def division(num_1, num_2):
-    result = int(num_1) / int(num_2)
-    print(f'{num_1} / {num_2} is equal to {result}')
-    return result
-
-def potencia(num_1, num_2):
-    result = int(num_1) ^ int(num_2)
-    print(f'{num_1} ^ {num_2} is equal to {result}')
-    return result
-
-def modulo(num_1, num_2):
-    result = int(num_1) % int(num_2)
-    print(f'{num_1} % {num_2} is equal to {result}')
-    return result
